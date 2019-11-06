37f03bb70f3ed4687337fc806f8c299ba48e3b5c
fabmeyer <fabian@meyerfoto.ch>
2019-11-06 11:58:49 +0100

    added mic input

--- noise/sketch.js
+++ noise/sketch.js
@@ -1,42 +1,61 @@
+let rand = getRand();
+let mic;
+let r = rand % 255;
+let g = 192;
+let b = 127;
+
 function setup() {
-  width = 2400;
-  height = 1000;
+  frameRate(24);
+  width = 720;
+  height = 540;
   createCanvas(width, height);
+  let myDiv = createDiv("click to start mic input");
+  myDiv.position(0, 0);
+  // Start the audio context on a click/touch event
+  userStartAudio().then(function() {
+    myDiv.remove();
+  });
+  mic = new p5.AudioIn();
+  mic.start();
 }
 
-let rand = Math.floor(Math.random() * Math.floor(10000));
+function getRand() {
+  return Math.floor(Math.random() * Math.floor(1080));
+}
 
 function draw() {
-  let r = 255;
-  let g = 192;
-  let b = 0;
-  let times = 255;
+  micLevel = Math.floor(Math.sqrt(mic.getLevel() * 100)) + 1;
+  console.log(micLevel);
 
-  background(r, g, b);
+  rand++;
+
+  background(0, 0, 0);
   stroke(0);
 
-  for (let i = rand - times; i < rand; i++) {
+  for (let i = rand - micLevel; i < rand; i++) {
     beginShape();
-    noiseSeed(i);
+    if (micLevel >= 2) {
+      noiseSeed(i / 10);
+      rand = getRand();
+    }
 
     let c = color(r, g, b);
     fill(c);
 
     for (let x = 0; x < width; x++) {
-      let nx = map(x, 0, width, 0, Math.sqrt(times));
-      let y = (height * noise(nx) + times / height) % height;
-      alpha(i % times);
-      stroke(i % times);
-      strokeWeight(i % times);
-      point(x, y + (i % times));
-      alpha(1);
+      let nx = map(x, 0, width, 0, Math.floor(micLevel) ** 3);
+      let y = height * noise(nx);
+      strokeWeight(Math.floor(micLevel));
+      stroke(r, 255, 255);
+      point(x, (y + i) % height);
+      strokeWeight(1);
       stroke(0);
-      strokeWeight(0.5);
-      vertex(x, y);
+      vertex(x, y % height);
     }
-    r = (r - times / 255) % 255;
+
+    r = Math.floor(r + micLevel) % 255;
     endShape(CLOSE);
   }
-  save();
-  save("mountains.png");
+  // save();
+  // save("mountains.png");
 }
